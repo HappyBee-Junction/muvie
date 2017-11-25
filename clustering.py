9d71a6b96e6d6198cec850199be922086c131872
omarnasir <momar.nasir@gmail.com>
2017-11-25 16:03:17 +0200

    fixed kmeans function definitions

--- clustering.py
+++ clustering.py
@@ -4,7 +4,6 @@ from senticnet.senticnet import Senticnet
 import re
 from scipy import stats
 import numpy as np
-import requests
 import build
 import senticnet4
 import collections
@@ -58,40 +57,6 @@ def processMovieData(dic):
         json.dump(output, outfile)
     with open('./movie_indices.txt', 'w') as outfile:
         json.dump(indices, outfile)
-lyrics = "There's a lady who's sure All that glitters is gold And she's buying a stairway to heaven When she gets there she knows If the stores are all closed With a word she can get what she came for Oh oh oh oh and she's buying a stairway to heaven There's a sign on the wall But she wants to be sure 'Cause you know sometimes words have two meanings In a tree by the brook There's a songbird who sings Sometimes all of our thoughts are misgiving Ooh, it makes me wonder Ooh, it makes me wonder There's a feeling I get When I look to the west And my spirit is crying for leaving In my thoughts I have seen Rings of smoke through the trees And the voices of those who standing looking Ooh, it makes me wonder Ooh, it really makes me wonder And it's whispered that soon, If we all call the tune Then the piper will lead us to reason And a new day will dawn For those who stand long And the forests will echo with laughter If there's a bustle in your hedgerow Don't be alarmed now It's just a spring clean for the May queen Yes, there are two paths you can go by But in the long run There's still time to change the road you're on And it makes me wonder Your head is humming and it won't go In case you don't know The piper's calling you to join him Dear lady, can you hear the wind blow And did you know Your stairway lies on the whispering wind And as we wind on down the road Our shadows taller than our soul There walks a lady we all know Who shines white light and wants to show How everything still turns to gold And if you listen very hard The tune will come to you at last When all are one and one is all To be a rock and not to roll And she's buying the stairway to heaven"
-
-def processLyrics():
-    dic = build.builddictionary()
-    model = processMovieData(dic)
-    #lyrics = requests.get("http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=15953433&apikey=a861047a0eb1272708e4d518bee92a6d")
-    lyrics_ = re.findall(r"[\w']+", lyrics.replace("'"," ")) 
-    with open('./stopwords.txt') as json_data:
-        STOPWORDS = json.load(json_data)
-    sn = Senticnet()
-    emotions = []
-    for word in lyrics_:
-        if word not in STOPWORDS and word in senticnet4.senticnet:
-            emotion = sn.moodtags(word)
-            for emo in emotion:
-                emotions.append(emo.replace("#",""))
-
-    #sortedList = sorted(stats.itemfreq(emotions), key=getCount)
-    emoLycList = stats.itemfreq(emotions)
-    emoLycDict = alphDict.copy()
-
-    for elt in emoLycList:
-        emoLycDict[elt[0]] = elt[1]
-    dicLyc = collections.OrderedDict(sorted(emoLycDict.items()))
-
-    #Compute list of frequencies out of dictionnary
-    orderedEmoLycList = []
-    for key in dicLyc.keys():
-        orderedEmoLycList.append(int(dicLyc[key]))
-    if np.linalg.norm(orderedEmoLycList) != 0.0:
-        final_emotions = orderedEmoLycList / np.linalg.norm(orderedEmoLycList)
-
-    #normalized(sortedList[::-1][:5]) #append keywords
-    print(final_emotions)
-
+        
 if __name__=="__main__":
-    processLyrics()
+    processMovieData(build.builddictionary())
--- kmeans.py
+++ kmeans.py
@@ -1,34 +1,65 @@
 from sklearn.cluster import KMeans
 from pandas import read_csv
+from scipy import stats
 import numpy as np
-import json
+import json, requests, collections
+import build, senticnet4
+from senticnet.senticnet import Senticnet
 
-def trainmodel(data):
-    #Fit the model on the data
-    kmeans = KMeans(n_clusters=50, random_state=0).fit(data)
-    #Assign each movie to a cluster
-    indices = kmeans.predict(data) #----pythonic indices
-    #Transpose by assigning each cluster its asscociated movies
-    res = [[] for i in range(50)]
-    for index,cluster in enumerate(indices):
-        res[cluster].append(index)
-    return kmeans, res
+alphDict = {'admiration': 0, 'anger': 0, 'disgust': 0, 'fear': 0, 'interest': 0 , 'joy': 0, 'sadness': 0, 'surprise': 0}
 
-def predict(model,lyrics):
-    #Give a new lyrics, predict the cluster it belongs to 
+#Read txt File containing the dataset
+with open('./movie_raw.txt') as json_data:
+    txt = json.load(json_data)
+with open('./movie_indices.txt') as json_data:
+    movie_indices = json.load(json_data)
+with open('./stopwords.txt') as json_data:
+    STOPWORDS = json.load(json_data)
+
+#Fit the model on the data
+kmeans = KMeans(n_clusters=50, random_state=0).fit(txt)
+#Assign each movie to a cluster
+indices = kmeans.predict(txt)  # ----pythonic indices
+#Transpose by assigning each cluster its asscociated movies
+res = [[] for i in range(50)]
+for index, cluster in enumerate(indices):
+    res[cluster].append(index)
+
+def processLyrics(lyrics):
+    dic = build.builddictionary()
+    sn = Senticnet()
+    emotions = []
+    for word in lyrics:
+        if word not in STOPWORDS and word in senticnet4.senticnet:
+            emotion = sn.moodtags(word)
+            for emo in emotion:
+                emotions.append(emo.replace("#", ""))
+    #sortedList = sorted(stats.itemfreq(emotions), key=getCount)
+    emoLycList = stats.itemfreq(emotions)
+    emoLycDict = alphDict.copy()
+
+    for elt in emoLycList:
+        emoLycDict[elt[0]] = elt[1]
+    dicLyc = collections.OrderedDict(sorted(emoLycDict.items()))
+
+    #Compute list of frequencies out of dictionnary
+    orderedEmoLycList = []
+    for key in dicLyc.keys():
+        orderedEmoLycList.append(int(dicLyc[key]))
+    if np.linalg.norm(orderedEmoLycList) != 0.0:
+        final_emotions = orderedEmoLycList / np.linalg.norm(orderedEmoLycList)
+    #normalized(sortedList[::-1][:5]) #append keywords
+    return final_emotions
+
+def predict(model, lyrics):
+    #Give a new lyrics, predict the cluster it belongs to
     center = model.predict(lyrics)
     return center
 
-def kmeans():
-    #Read txt File containing the movie dataset
-    with open('./movie_raw.txt') as json_data:
-        txt = json.load(json_data)
-    with open('./movie_indices.txt') as json_data:
-        movie_indices = json.load(json_data)
-    #Train and predict the lyrics
-    model, clusters = trainmodel(txt)
-    lyrics = np.array(lyrics_raw).reshape(1,-1)
-    predicted_label = predict(model,lyrics)
+def kmeans(lyrics):
+    lyrics_emotions = processLyrics(lyrics)
+    lyrics = np.array(lyrics_emotions).reshape(1, -1)
+    predicted_label = predict(model, lyrics)
     #Find the indices of the movies in that cluster
     arr_movies = clusters[predicted_label[0]]
     #Computer L2 Norm and find the 5 smallest norm values
