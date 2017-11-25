33e573fa53de6cfe732615dafa75403301fe7790
omarnasir <momar.nasir@gmail.com>
2017-11-25 15:46:13 +0200

    fixed kmeans algorithm

--- kmeans.py
+++ kmeans.py
@@ -3,17 +3,15 @@ from pandas import read_csv
 import numpy as np
 import json
 
-lyrics = [0.58970719550332,0.016380882276275,0.863911783129826,0.535381599410218,0.556402999059265,0.28556406100863,0.912163440923888,0.418320719872901]
-
-def trainmodel(data, movie_indices):
+def trainmodel(data):
     #Fit the model on the data
     kmeans = KMeans(n_clusters=50, random_state=0).fit(data)
     #Assign each movie to a cluster
-    indices = kmeans.predict(data)
+    indices = kmeans.predict(data) #----pythonic indices
     #Transpose by assigning each cluster its asscociated movies
     res = [[] for i in range(50)]
     for index,cluster in enumerate(indices):
-        res[cluster].append(movie_indices[index])
+        res[cluster].append(index)
     return kmeans, res
 
 def predict(model,lyrics):
@@ -21,23 +19,24 @@ def predict(model,lyrics):
     center = model.predict(lyrics)
     return center
 
-def main():
+def kmeans():
     #Read txt File containing the movie dataset
     with open('./movie_raw.txt') as json_data:
         txt = json.load(json_data)
     with open('./movie_indices.txt') as json_data:
         movie_indices = json.load(json_data)
     #Train and predict the lyrics
-    model, clusters = trainmodel(txt, movie_indices)
-    predicted_label = predict(model,lyrics = np.array(lyrics).reshape(1,-1))
+    model, clusters = trainmodel(txt)
+    lyrics = np.array(lyrics_raw).reshape(1,-1)
+    predicted_label = predict(model,lyrics)
     #Find the indices of the movies in that cluster
-    cluster_movies = clusters[predicted_label[0]]
+    arr_movies = clusters[predicted_label[0]]
     #Computer L2 Norm and find the 5 smallest norm values
     scores = []
-    for movie in cluster_movies:
-        scores.append(np.linalg.norm(txt.values[movie] - lyrics))
-    print(np.array(scores).argsort()[::1][:5])
-
-
-if __name__ == "__main__":
-    main()
+    for movie in arr_movies:
+        scores.append(np.linalg.norm(np.array(txt[movie]) - lyrics))
+    kmeans_indices = np.array(scores).argsort()[::1][:5]
+    movie_ids = []
+    for index in kmeans_indices:
+        movie_ids.append(movie_indices[arr_movies[index]])
+    return movie_ids
